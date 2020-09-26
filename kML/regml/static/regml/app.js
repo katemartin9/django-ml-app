7cd30a536746a2f8176434a7af18095d851eb948
Kate Martin <katmartin.uk@gmail.com>
2020-09-26 22:02:54 +0100

    calling api with js promise and creating svgs

--- kML/regml/static/regml/app.js
+++ kML/regml/static/regml/app.js
@@ -1,31 +1,24 @@
+fetch('http://127.0.0.1:8000/regml/model-options/boston project/api')
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+      const margin = { top: 30, right: 20, bottom: 30, left: 50 };
+      const width = 300 - margin.left - margin.right;
+      const height = 300 - margin.top - margin.bottom;
 
-function formatTable() {
-    let elems = document.getElementsByTagName('td')
-    Array.from(elems).forEach(elem => {
-        debugger;
-        typeof elem.innerText
-    elem.innerText = Number(elem.innerText).toFixed(1);
+      function createSvg() {
+      d3
+      .select("body")
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    }
+      var corrMatrix = createSvg()
+      var linePlots = createSvg()
+      debugger;
     });
-}
 
-//formatTable()
-
-function apiData(projectName, id) {
-  return new Promise(function(resolve, reject) {
-    d3.json(`'http://http://127.0.0.1:8000/regml/model-options/'${projectName}/${id}`, function(error, data) {
-      if (error) {
-      	reject(error);
-      } else {
-      	resolve(data);
-      }
-    });
-  });
-}
-
-apiData('boston project', 'api')
-  .then(function (data) {
-    console.log(data);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
